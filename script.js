08a6937550c81d62fa03fb516fb0543b2d8a6a74
snautssol <snautssol@gmail.com>
2024-12-16 17:26:38 -0500

    Create script.js
    create script.js file to sloths nauts website

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', async () => {
+    if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
+        const provider = new Web3(window['ethereum'] || window.web3.currentProvider);
+        window.web3 = new Web3(provider);
+    } else {
+        console.log('No Web3 provider detected.');
+    }
+
+    const connectWalletButton = document.getElementById('connectWallet');
+
+    connectWalletButton.addEventListener('click', async () => {
+        if (window.web3) {
+            try {
+                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+                console.log('Connected account:', accounts[0]);
+            } catch (error) {
+                console.error('Failed to connect wallet:', error);
+            }
+        } else {
+            console.error('Web3 not initialized.');
+        }
+    });
+});
