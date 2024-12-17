58ecb84c9169de3ef647704952c3d225f25c2f71
snautssol <snautssol@gmail.com>
2024-12-16 23:48:47 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middleware para servir archivos estáticos desde el directorio build
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/api', (req, res) => {
+    res.send('API funcionando correctamente');
+});
+
+// Sirve la aplicación React para todas las rutas que no coincidan con /api
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
